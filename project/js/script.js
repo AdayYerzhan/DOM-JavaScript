3afb3d635c4bea401a20f1ae168c547ba9ddf492
Yerzhan <yerzhan.aday@gmail.com>
2023-05-09 15:48:43 +0600

    working with DOM events

--- project/js/script.js
+++ project/js/script.js
@@ -1,66 +1,83 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан Logan",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан Logan",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
-const adv = document.querySelectorAll('.promo__adv img'),
-    poster = document.querySelector('.promo__bg'),
-    genre = poster.querySelector('.promo__genre'),
-    movieList = document.querySelector('.promo__interactive-list');
+    const adv = document.querySelectorAll('.promo__adv img'),
+        poster = document.querySelector('.promo__bg'),
+        genre = poster.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]'),
+        btn = document.querySelector('button');
 
-adv.forEach(item => {
-    item.remove();
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        if (addInput.value.length >= 21) {
+            movieDB.movies.push(`${addInput.value.slice(0, 20)}...`);
+        } else {
+            if (addInput.value) {
+                movieDB.movies.push(addInput.value);
+            }
+        }
+
+        checkbox.checked ? console.log('Добавляем любимый фильм') : null;
+        sortArr(movieDB.movies);
+
+        createMovieList(movieDB.movies, movieList);
+        event.target.reset();
+    });
+
+    function deleteAdv(arr) {
+        arr.forEach(item => {
+            item.remove();
+        });
+    }
+
+    const makeChanges = () => {
+        genre.textContent = 'драма';
+
+        poster.style.backgroundImage = 'url(./img/bg.jpg)';
+    };
+
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+    function createMovieList(films, parent) {
+        parent.innerHTML = '';
+        films.sort();
+        
+        films.forEach((item, i) => {
+            movieList.innerHTML += `
+                <li class="promo__interactive-item">${i + 1}. ${item}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                // delete movieDB.movies[i];
+                movieDB.movies.splice(i, 1);
+                createMovieList(movieDB.movies, movieList);
+            });
+        });
+    }
+
+
+    deleteAdv(adv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
 });
-
-genre.textContent = 'драма';
-
-poster.style.backgroundImage = 'url(./img/bg.jpg)';
-
-
-movieDB.movies.sort();
-
-// films.querySelectorAll('.promo__interactive-item').forEach( item => {
-//     item.remove();
-// });
-
-movieList.innerHTML = '';
-
-// movieDB.movies.forEach((item, i) => {
-//     movieList.insertAdjacentHTML('beforeend', `
-//         <li class="promo__interactive-item">${i + 1}. ${item}
-//             <div class="delete"></div>
-//         </li>
-//     `);
-// });
-
-movieDB.movies.forEach((item, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1}. ${item}
-            <div class="delete"></div>
-        </li>
-    `;
-});
-
-
-
